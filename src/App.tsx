64d95c3b401ee9d48fbe31e7d6a10b1da0cab686
Mace <153683321+127msafran@users.noreply.github.com>
2026-09-23 13:48:47 +0000

    Starting Over Pt. 2

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <section>
+      <div>
+        <h1>Mason Safran</h1>
+        <p>Aspiring Software Engineer</p>
+        <p><a href="https://github.com/127msafran">Github</a> | <a>LinkedIn</a> | <a>Email</a></p>
+      </div>
+    </section>
+  )
+}
